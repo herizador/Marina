5a76b4f76d2ce50dc8d7c546bbc52382da716b39
herizador <155181668+herizador@users.noreply.github.com>
2025-02-12 22:48:15 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "san-valentin-v1";
+const STATIC_ASSETS = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/script.js",
+    "/manifest.json",
+    "/icon.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => 
+            Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+        )
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => cachedResponse || fetch(event.request))
+    );
+});
